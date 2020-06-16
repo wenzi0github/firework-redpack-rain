ee2595c6957293b6f3619e596d78a4bebc21ded3
skeetershi(师少兵) <skeetershi@tencent.com>
2020-06-16 22:38:45 +0800

    fix(webpack): add globalObject when libraryTarget is umd #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const packageConfig = require( './package' );
+const packageConfig = require("./package");
 
 // https://webpack.docschina.org/configuration
 module.exports = {
@@ -15,6 +15,7 @@ module.exports = {
         libraryExport: "default",
         library: "User",
         libraryTarget: "umd",
+        globalObject: "this",
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
@@ -29,5 +30,11 @@ module.exports = {
             },
         ],
     },
-    plugins: [new webpack.BannerPlugin(`User v${packageConfig.version}\nlast update: ${new Date().toLocaleString()}\nauthor: skeetershi`)],
+    plugins: [
+        new webpack.BannerPlugin(
+            `User v${
+                packageConfig.version
+            }\nlast update: ${new Date().toLocaleString()}\nauthor: skeetershi`
+        ),
+    ],
 };
